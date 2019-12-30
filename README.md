c7744436bebffabf14e8e57e461ab38e7cab8e5b
Brandon Sachtleben <5560621+BrandonRiemann@users.noreply.github.com>
2019-12-30 02:13:57 -0500

    Updated README

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # MinIRC
- Minimal IRC client using ncurses written in C.
+ Minimalistic IRC client using ncurses written in C.
+
+![](https://i.gyazo.com/781b1b70c41df7652e1261ec841755a3.gif)
--- bot.h
+++ bot.h
@@ -6,14 +6,14 @@
 #define BOT_NAME    "MinIRC"
 #define BOT_VERSION "1.0"
 #define BOT_AUTHOR  "Brandon Sachtleben"
-#define BOT_DESC    "Minimal IRC client using ncurses"
+#define BOT_DESC    "Minimalistic IRC client using ncurses"
 
 #define TWITCH_IRC_HOST  "irc.chat.twitch.tv"
 #define TWITCH_IRC_PORT  "6667"
 
-#define IRC_NICK         "subhype"
-#define IRC_PASS         "oauth:asj10om0cw01rqqujt02jdec4vz1d1"
-#define IRC_CHANNEL      "subhype"
+#define IRC_NICK         "brandonriemann"
+#define IRC_PASS         "oauth:651q0zzssi9zl6kmk0iwwh9n6reyny"
+#define IRC_CHANNEL      "vinesauce"
 
 int print_bot_strings( WindowContext* );
 
